8c119631d0637de0511f52f439bdcb258579ef55
farhaan-ali <farhaan.ali@brickvest.com>
2021-06-08 08:56:56 +0100

    Contract: Basic Farhaan Token contract
    This creates a token with a total supply of 100,000 tokens
    with the ability to transfer and check the balance.

--- contracts/Token.sol
+++ contracts/Token.sol
@@ -3,6 +3,23 @@ pragma solidity ^0.8.3;
 
 import "hardhat/console.sol";
 
-
 contract Token {
+  string public name = "Farhaan Token";
+  string public symbol = "FT";
+  uint public totalSupply = 100000;
+  mapping(address => uint) balances;
+
+  constructor() {
+    balances[msg.sender] = totalSupply;
+  }
+
+  function transfer(address to, uint amount) external {
+    require(balances[msg.sender] >= amount, "Not enough tokens");
+    balances[msg.sender] -= amount;
+    balances[to] += amount;
+  }
+
+  function balanceOf(address account) external view returns (uint) {
+    return balances[account];
+  }
 }
